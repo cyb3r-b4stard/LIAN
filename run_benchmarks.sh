043eba78fa31b73db2ed12734f12e39ac9569402
Ivan Lomikovskiy <hollowcoder@yandex.ru>
2024-11-24 14:05:55 +0300

    Optimized benchmark script to avoid excess work. Benchmarks for algorithms execute in separate processes.

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -3,7 +3,7 @@
 # Constants for coloring output
 RED='\033[0;31m'
 GREEN='\033\e[0;32m'
-NC='\033[0m' 
+NO_COLOR='\033[0m' 
 
 # List of necessary python packages for CI/CD
 packages=('pandas' 'matplotlib')
@@ -12,11 +12,11 @@ packages=('pandas' 'matplotlib')
 for package in ${packages[@]}
 do
     pip3 show $package 1>/dev/null 
-    if [ $? == 0 ]; 
+    if [ $? == 0 ]
     then
-        echo -e ${GREEN}OK${NC}: $package is installed!
+        echo -e ${GREEN}OK${NO_COLOR}: $package is installed!
     else
-        echo -e ${RED}ERROR${NC}: $package is not installed! ; exit 1
+        echo -e ${RED}ERROR${NO_COLOR}: $package is not installed! ; exit 1
     fi
 done
 
@@ -24,25 +24,53 @@ done
 mkdir ./basiclian && git clone https://github.com/cyb3r-b4stard/basicLIAN.git basiclian/
 
 # Create directories for logs
-mkdir ./benchmark/out ; mkdir ./benchmark/out/logs_basic ; mkdir ./benchmark/out/logs_modified
+mkdir ./benchmark/out
+rm -r ./benchmark/out/logs_basic    ; mkdir ./benchmark/out/logs_basic
+rm -r ./benchmark/out/logs_modified ; mkdir ./benchmark/out/logs_modified
 
 # Create directories for resources
-mkdir ./benchmark/resources/xml_basic ; mkdir ./benchmark/resources/xml_modified ; python3 benchmark/generate_missions.py
+# mkdir ./benchmark/resources/xml_basic ; mkdir ./benchmark/resources/xml_modified ; python3 benchmark/generate_missions.py
+ 
+if [ ! -d  './benchmark/resources/xml_basic' ] || [ ! -d './benchmark/resources/xml_modified' ]
+then
+    if [ ! -d  './benchmark/resources/xml_basic' ]
+    then
+        mkdir ./benchmark/resources/xml_basic
+    fi
+    if [ ! -d './benchmark/resources/xml_modified' ]
+    then 
+        mkdir ./benchmark/resources/xml_modified
+    fi
+    python3 benchmark/generate_missions.py
+fi
 
 # Build algorithms
-cd ./basiclian && cmake . && make && cd .. ; cmake . && make
+if [ ! -f './basiclian/LianSearch' ]
+then
+    cd ./basiclian ; cmake . ; make ; cd ..
+fi &
+cmake . ; make &
+wait
 
-# Iterate over missions for basic algorithm
-for file in ./benchmark/resources/xml_basic/*.xml
-do
-    ./basiclian/LianSearch $file
-done
+function benchmark(){
+    if [ $1 == 'basic' ] # Iterate over missions for basic LIAN
+    then
+        for file in ./benchmark/resources/xml_basic/*.xml
+        do
+            ./basiclian/LianSearch $file
+        done
+    else # Iterate over missions for modified LIAN
+        for file in ./benchmark/resources/xml_modified/*.xml
+        do
+            ./LianSearch $file
+        done
+    fi
+}
 
-# Iterate over missions for modified algorithm
-for file in ./benchmark/resources/xml_modified/*.xml
-do
-    ./LianSearch $file
-done
+# Run benchmarks in separate processes
+benchmark 'basic'    & 
+benchmark 'modified' &
+wait
 
 # Move logs into benchmark/out directory
 mv ./benchmark/resources/xml_basic/*_log.xml ./benchmark/out/logs_basic/
